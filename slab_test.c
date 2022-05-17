7feab549865af7a362f1733b1361a3a225a1d49f
codeReaper2001 <2210086948@qq.com>
2022-05-17 23:33:15 +0800

    修正测试代码

--- slab_test.c
+++ slab_test.c
@@ -10,22 +10,22 @@ int main(int argc, char* argv[]) {
 
     char* m2 = (char*)my_malloc(29);
     printf(1, "m2 virtual address:; %p\n", m2);
-    m2[0] = 'A';
+    m2[0] = 'B';
     printf(1, "m2[0] data: %c\n\n", m2[0]);
 
     char* m3 = (char*)my_malloc(28);
     printf(1, "m3 virtual address:; %p\n", m3);
-    m3[0] = 'A';
+    m3[0] = 'C';
     printf(1, "m3[0] data: %c\n\n", m3[0]);
+
+
+    my_free(m2);
+    printf(1, "free virtual address m2: %p\n\n", m2);
 
     char* m4 = (char*)my_malloc(27);
     printf(1, "m4 virtual address:; %p\n", m4);
-    m4[0] = 'A';
+    m4[0] = 'D';
     printf(1, "m4[0] data: %c\n\n", m4[0]);
 
-    my_free(m1);
-    my_free(m2);
-    my_free(m3);
-    my_free(m4);
     exit();
 }
